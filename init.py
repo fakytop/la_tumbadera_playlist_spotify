3cd0c3187bbd6322eae2bf281a8b34d7ca49f22b
Fakytop <prgonzalo94@gmail.com>
2026-03-18 14:48:29 -0300

    se agrega filtros de albums por fechas

--- init.py
+++ init.py
@@ -1,10 +1,12 @@
 import os
 import requests
 import json
+
 from dotenv import load_dotenv
 
 from methods.get_token import get_token
 from methods.get_albums import get_albums_from_artist
+from methods.get_albums import analyze_dates_releases
 
 load_dotenv()
 
@@ -17,4 +19,25 @@ URL_BASE = "https://api.spotify.com/v1/"
 token = get_token(CLIENT_ID,SECRET_ID)
 
 albums = get_albums_from_artist(URL_BASE,'6uJKnn4CV4IIop8mg4kCUy',token)
-# print(json.dumps(albums,indent=4,ensure_ascii=False))
+
+newest_releases = []
+last_three_months_releases = []
+last_three_years_releases = []
+
+analyze_dates_releases(albums,newest_releases,last_three_months_releases,last_three_years_releases)
+
+folder_name = "responses_api"
+os.makedirs(folder_name,exist_ok=True)
+
+newest_filepath=os.path.join(folder_name,"newest_releases.json")
+last_months_filepath=os.path.join(folder_name,"last_three_months_releases.json")
+last_years_filepath=os.path.join(folder_name,"last_three_years_releases.json")
+
+with open(newest_filepath,"w",encoding="utf-8") as f:
+    json.dump(newest_releases,f,indent=4,ensure_ascii=False)
+    
+with open(last_months_filepath,"w",encoding="utf-8") as f:
+    json.dump(last_three_months_releases,f,indent=4,ensure_ascii=False)
+    
+with open(last_years_filepath,"w",encoding="utf-8") as f:
+    json.dump(last_three_years_releases,f,indent=4,ensure_ascii=False)
--- methods/get_albums.py
+++ methods/get_albums.py
@@ -1,7 +1,7 @@
 import requests
+from datetime import date,timedelta
+from datetime import datetime
 
-def add_albums(total_items,items):
-    total_items.extend(items)
 
 def get_albums_from_artist(url_base,artist_id,token):
     
@@ -24,8 +24,50 @@ def get_albums_from_artist(url_base,artist_id,token):
             starting_offset = response["offset"]
             offset += items.__len__()
             print(f"Guardando en la lista los siguientes {offset - starting_offset} albums")
-            add_albums(total_items,response["items"])
+            total_items.extend(response["items"])
         return total_items
     except requests.exceptions.HTTPError as err:
         print(f"❌ Error de la API: {response.status_code} - {response.text}")
         raise SystemExit(err)
+
+def get_date(date):
+    date_time = datetime.strptime(date,"%Y-%m-%d")
+    return date_time.date()
+
+def add_filtered_albums_between_dates(filtered_list,album,album_date,newest_date,oldest_date):
+    if album_date >= oldest_date and album_date <= newest_date:
+        filtered_list.append(album)
+        print(f"💾 Album [{album["name"]}] guardado con éxito. Fecha de lanzamiento: {album_date}")
+    else:
+        print(f"🚫 El álbum [{album["name"]}] no fue guardado. La fecha de lanzamiento: {album_date} no está comprendida entre [{newest_date} - {oldest_date}]")
+
+def get_albums_between_dates(albums,newest_time,oldest_time):
+    filtered_albums = []
+    for album in albums: 
+        if album["release_date_precision"] == 'day':
+            album_date = get_date(album["release_date"])
+            add_filtered_albums_between_dates(filtered_albums,album,album_date,newest_time,oldest_time)
+        elif album["release_date_precision"] == 'year':
+            add_filtered_albums_between_dates(filtered_albums,album,album["release_date"],newest_time.year(),oldest_time.year())
+        else:
+            print(f"⛔ Precision de fecha mal definida. Álbum: [{album["name"]}] - [{album["release_date"]}]")
+    return filtered_albums
+
+def get_filtered_albums(albums,newest_time,oldest_time):
+    return get_albums_between_dates(albums,newest_time,oldest_time)
+
+def analyze_dates_releases(albums,newest_releases,last_three_months_releases,last_three_years_releases):
+    today = date.today()
+    oldest_time = today - timedelta(days=15)
+    print("📈 Analizando los nuevos lanzamientos.")
+    newest_releases.extend(get_filtered_albums(albums,today,oldest_time))
+
+    today = oldest_time - timedelta(days=1)
+    oldest_time = today - timedelta(days=90)
+    print("📈 Analizando los lanzamientos de los últimos 3 meses.")
+    last_three_months_releases.extend(get_filtered_albums(albums,today,oldest_time))
+
+    today = oldest_time -timedelta(days=1)
+    oldest_time = today - timedelta(days=1095)
+    print("📈 Analizando los lanzamientos de los últimos 3 años.")
+    last_three_years_releases.extend(get_filtered_albums(albums,today,oldest_time))
