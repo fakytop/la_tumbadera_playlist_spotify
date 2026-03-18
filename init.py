466396a121d6f35083d88fc171dd322b1b46e67a
Fakytop <prgonzalo94@gmail.com>
2026-03-18 15:20:08 -0300

    Se agrega iteración para todos los artistas, y se controla que no se repita el album

--- init.py
+++ init.py
@@ -17,14 +17,32 @@ URL_BASE = "https://api.spotify.com/v1/"
 
 
 token = get_token(CLIENT_ID,SECRET_ID)
-
-albums = get_albums_from_artist(URL_BASE,'6uJKnn4CV4IIop8mg4kCUy',token)
+artists = [ 
+    '6uJKnn4CV4IIop8mg4kCUy' # Luana
+    ,'2WZW6Gsj5R7JlRNFikJvPE' # Juanjo Morgade
+    ,'2UnGWnrqDZPjWNVRWj5Mae' # Maxi Alvarez
+    ,'5TeBsszZQTyqBX4eDHdtNx' # La Nueva Escuela
+    ,'4wNFb9W3qEbAgwk0Ln8iea' # La Revulsiva
+    ,'4sXSRSA4DORtoEz8WDqkWa' # La Dosis
+    ,'3Pohtvl5MO8eQpaZVOrhUS' # La Deskarga
+    ,'6WrhdoSkwNxeTzjtdLfnPU' # Los Negroni
+    ,'4X6BwKYD52c4HHDyUqetfU' # Denis Elias
+    ,'5pjGkQyoECqaaQLMn9z6he' # Damian Lescano
+    ,'0annXurxTVJKs4QqOqdwGR' # El Gucci
+    ,'2weGfox1CWg9HmmwDgcjey' # Marcos Da Costa
+    ,'6nqh6VtIRmLWvtv6suXdiq' # Mariano Bermúdez
+    ,'6bOZtDVI19rOqC4NWihcea' # Los Pikantes
+]
 
 newest_releases = []
 last_three_months_releases = []
 last_three_years_releases = []
+unique_ids_albums = set()
 
-analyze_dates_releases(albums,newest_releases,last_three_months_releases,last_three_years_releases)
+for artist in artists:
+    albums = get_albums_from_artist(URL_BASE,artist,token)
+    analyze_dates_releases(albums,newest_releases,last_three_months_releases,last_three_years_releases,unique_ids_albums)
+
 
 folder_name = "responses_api"
 os.makedirs(folder_name,exist_ok=True)
--- methods/get_albums.py
+++ methods/get_albums.py
@@ -34,40 +34,50 @@ def get_date(date):
     date_time = datetime.strptime(date,"%Y-%m-%d")
     return date_time.date()
 
-def add_filtered_albums_between_dates(filtered_list,album,album_date,newest_date,oldest_date):
+def add_filtered_albums_between_dates(filtered_list,album,album_date,newest_date,oldest_date,unique_ids_albums):
     if album_date >= oldest_date and album_date <= newest_date:
         filtered_list.append(album)
         print(f"💾 Album [{album["name"]}] guardado con éxito. Fecha de lanzamiento: {album_date}")
+        unique_ids_albums.add(album["id"])
     else:
         print(f"🚫 El álbum [{album["name"]}] no fue guardado. La fecha de lanzamiento: {album_date} no está comprendida entre [{newest_date} - {oldest_date}]")
 
-def get_albums_between_dates(albums,newest_time,oldest_time):
+def get_albums_between_dates(albums,newest_time,oldest_time,unique_ids_albums):
     filtered_albums = []
     for album in albums: 
-        if album["release_date_precision"] == 'day':
-            album_date = get_date(album["release_date"])
-            add_filtered_albums_between_dates(filtered_albums,album,album_date,newest_time,oldest_time)
-        elif album["release_date_precision"] == 'year':
-            add_filtered_albums_between_dates(filtered_albums,album,album["release_date"],newest_time.year(),oldest_time.year())
+        if album["id"] not in unique_ids_albums:
+            if album["release_date_precision"] == 'day':
+                album_date = get_date(album["release_date"])
+                add_filtered_albums_between_dates(filtered_albums,album,album_date,newest_time,oldest_time,unique_ids_albums)
+            elif album["release_date_precision"] == 'year':
+                add_filtered_albums_between_dates(filtered_albums,album,int(album["release_date"]),newest_time.year,oldest_time.year,unique_ids_albums)
+            else:
+                print(f"⛔ Precision de fecha mal definida. Álbum: [{album["name"]}] - [{album["release_date"]}]")
         else:
-            print(f"⛔ Precision de fecha mal definida. Álbum: [{album["name"]}] - [{album["release_date"]}]")
+            print(f"⛔ El álbum ya fue agregado a las listas. Álbum: [{album["name"]}] - [{album["release_date"]}]")
     return filtered_albums
 
-def get_filtered_albums(albums,newest_time,oldest_time):
-    return get_albums_between_dates(albums,newest_time,oldest_time)
+def get_filtered_albums(albums,newest_time,oldest_time,unique_ids_albums):
+    return get_albums_between_dates(albums,newest_time,oldest_time,unique_ids_albums)
 
-def analyze_dates_releases(albums,newest_releases,last_three_months_releases,last_three_years_releases):
+def analyze_dates_releases(albums,newest_releases,last_three_months_releases,last_three_years_releases,unique_ids_albums):
     today = date.today()
     oldest_time = today - timedelta(days=15)
-    print("📈 Analizando los nuevos lanzamientos.")
-    newest_releases.extend(get_filtered_albums(albums,today,oldest_time))
+    print("-----------------------------------------------------------------------------------------")
+    print("📈                       ANALIZANDO NUEVOS LANZAMIENTOS")
+    print("-----------------------------------------------------------------------------------------")
+    newest_releases.extend(get_filtered_albums(albums,today,oldest_time,unique_ids_albums))
 
     today = oldest_time - timedelta(days=1)
     oldest_time = today - timedelta(days=90)
-    print("📈 Analizando los lanzamientos de los últimos 3 meses.")
-    last_three_months_releases.extend(get_filtered_albums(albums,today,oldest_time))
+    print("-----------------------------------------------------------------------------------------")
+    print("📈               ANALIZANDO LOS LANZAMIENTOS DE LOS ÚLTIMOS 3 MESES.")
+    print("-----------------------------------------------------------------------------------------")
+    last_three_months_releases.extend(get_filtered_albums(albums,today,oldest_time,unique_ids_albums))
 
     today = oldest_time -timedelta(days=1)
     oldest_time = today - timedelta(days=1095)
-    print("📈 Analizando los lanzamientos de los últimos 3 años.")
-    last_three_years_releases.extend(get_filtered_albums(albums,today,oldest_time))
+    print("-----------------------------------------------------------------------------------------")
+    print("📈               ANALIZANDO LOS LANZAMIENTOS DE LOS ÚLTIMOS 3 AÑOS.")
+    print("-----------------------------------------------------------------------------------------")
+    last_three_years_releases.extend(get_filtered_albums(albums,today,oldest_time,unique_ids_albums))
